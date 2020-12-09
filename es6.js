584816c8122c72d6ddd190bb3d5e8e8dbc60157b
Girard Andrew Tabañag <girard@tabanag.com>
2020-12-10 00:18:15 +1100

    Day 017

--- es6.js
+++ es6.js
@@ -146,9 +146,55 @@ result += "<br>" + a + ", " + b;
 
 // Day 017
 // Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a, b, ...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
+
+result += "<br>" + arr;
+
 // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0; 
+// Only change code above this line
+
+result += "<br>" + half;
+
 // Create Strings using Template Literals
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    failureItems.push
+    arr.forEach();
+    // Only change code above this line
+
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+
 // Write Concise Object Literal Declarations Using Object Property Shorthand
 // Write Concise Declarative Functions with ES6
 
+//result += "<br>" + 
 document.getElementById("result").innerHTML = result;
